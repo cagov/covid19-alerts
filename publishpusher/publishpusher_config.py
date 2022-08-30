2fe9a91780da18cd20ddbfe4904f3adbf513deaf
Jim Bumgardner <jbum@jbum.com>
2022-08-29 19:36:38 -0700

    Changed trigger URL.

--- publishpusher/publishpusher_config.py
+++ publishpusher/publishpusher_config.py
@@ -3,4 +3,5 @@
 push_interval_secs = 60*10
 first_active_hour = 8
 last_active_hour = 19
-trigger_url = 'https://fa-cdt-covid19-d-001.azurewebsites.net/PantheonService'
+# trigger_url = 'https://fa-cdt-covid19-d-001.azurewebsites.net/PantheonService'
+trigger_url = 'https://fa-go-covid-data-cron-001.azurewebsites.net/PantheonService'
