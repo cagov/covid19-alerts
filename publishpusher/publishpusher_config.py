28c69c1561c0315226e04ff7de944e61fc617e9d
Jim Bumgardner <jbum@jbum.com>
2023-03-07 11:10:23 -0800

    Migrated pantheon service to AWS Lambda.

--- publishpusher/publishpusher_config.py
+++ publishpusher/publishpusher_config.py
@@ -3,5 +3,5 @@
 push_interval_secs = 60*10
 first_active_hour = 8
 last_active_hour = 19
-# trigger_url = 'https://fa-cdt-covid19-d-001.azurewebsites.net/PantheonService'
-trigger_url = 'https://fa-go-covid-data-cron-001.azurewebsites.net/PantheonService'
+# trigger_url = 'https://fa-go-covid-data-cron-001.azurewebsites.net/PantheonService'
+trigger_url = 'https://d1nud1wfld.execute-api.us-west-1.amazonaws.com/'
